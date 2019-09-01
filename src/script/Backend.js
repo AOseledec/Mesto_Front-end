237628fb52653676c71f3646ea58e9710b587362
AOseledec <artemij.oseledets@yandex.ru>
2019-09-01 12:39:31 +0300

    Изменено наименование переменных

--- src/script/Backend.js
+++ src/script/Backend.js
@@ -1,16 +1,16 @@
 import {Api} from './Api.js'
 
 const url = {
-    base: 'https://praktikum.tk/cohort1',
-    urlUsers: '/users',
-    token: '05085b6d-94ca-4d8c-9b9c-a218a21e8eeb',
-    urlMe: '/users/me',
-    urlCards: '/cards',
+    URL_BASE: 'https://praktikum.tk/cohort1',
+    URL_USERS: '/users',
+    TOKEN: '05085b6d-94ca-4d8c-9b9c-a218a21e8eeb',
+    URL_ME: '/users/me',
+    URL_CARDS: '/cards',
 }
 export const api = new Api({
-  baseUrl: url.base,
+  URL_BASE: url.URL_BASE,
   headers: {
-    authorization: url.token,
+    authorization: url.TOKEN,
     'Content-Type': 'application/json'
   }
 });
