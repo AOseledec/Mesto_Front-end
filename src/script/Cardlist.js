c1794d6931051cc9a58923e253142e877e0909bf
AOseledec <artemij.oseledets@yandex.ru>
2019-09-01 14:59:51 +0300

    cleaning code

--- src/script/Cardlist.js
+++ src/script/Cardlist.js
@@ -4,24 +4,16 @@ import {Popup} from './Popup.js'
 export class CardList {
     constructor(container, /*listCards,*/ popup) {
       this.container  = container;
-      // this.listCards  = listCards;
       this.popupImage = new Popup(popup);
       
       this.container.addEventListener('click', this.openImagePopup.bind(this));
-      // this.render();
     }
   
     addCard(title, url, owner_id,card_id, user_id) {
       const { cardElement } = new Card(title, url, owner_id,card_id, user_id);
       this.container.appendChild(cardElement);
     }
-  
-    // render() {
-    //   this.listCards.forEach(element => {
-    //     this.addCard(element.name, element.link);
-    //   });
-    // }
-  
+    
     openImagePopup(event){
       if(event.target.classList.contains('place-card__image')){
         this.popupImage.openImage(event.target.style.backgroundImage.slice(5,-2),this.popupImage);
